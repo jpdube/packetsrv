a9dac626b2a4dc13721e3d3f1dc4f97552ff4660
JP Dube <jpdube@numa-informatique.com>
2022-08-10 14:52:00 -0400

    Minor tweaks

--- dbase/packet_search.py
+++ dbase/packet_search.py
@@ -6,8 +6,8 @@ from typing import Dict
 from packet.layers.packet_decode import PacketDecode
 from packet.utils.print_hex import format_hex
 
-db_filename = "/Users/jpdube/hull-voip/db/index.db"
-pcap_path = "/Users/jpdube/hull-voip/db/pcap"
+db_filename = "/home/jpdube/hull-voip/db/index.db"
+pcap_path = "/home/jpdube/hull-voip/db/pcap"
 
 PCAP_GLOBAL_HEADER_SIZE = 24
 PCAP_PACKET_HEADER_SIZE = 16
@@ -45,7 +45,7 @@ def packet_search(params):
                 # result.append(pb)
                 result.append(True)
 
-    print(f"Found:{len(result)}, Time:{datetime.now() - start_time}")
+    # print(f"Found:{len(result)}, Time:{datetime.now() - start_time}")
     return result
 
 
@@ -53,7 +53,7 @@ def search_parallel():
     pool = Pool()
     flist = []
     start_time = datetime.now()
-    for i in range(50):
+    for i in range(400):
         params = {
             "file": i,
             "filter": ("ip.src", 0xc0a80301)
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from api.server2 import start
 from dbase.dbcache import IndexCache
+# from dbase.packet_search import search_parallel
 
 if __name__ == "__main__":
+    # search_parallel()
     IndexCache.init()
     start()
     # uvicorn.run(app=app, host="127.0.0.1", port=8080)
