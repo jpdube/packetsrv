e941eb64084a796efaf552f6620423d73bf8a3c5
JP Dube <jpdube@numa-informatique.com>
2022-09-28 12:06:40 -0400

    Created indexer to import raw pcap files in the database

--- dbase/indexer.py
+++ dbase/indexer.py
@@ -1,31 +1,28 @@
 import sqlite3
-import os
 from datetime import datetime
 from multiprocessing import Pool
 from struct import unpack
-from packet.layers.packet_builder import PacketBuilder
+from packet.layers.packet_decode import PacketDecode
 
-# from packet.layers.packet_decode import PacketDecode
-from packet.layers.layer_type import LayerID
-from packet.utils.print_hex import format_hex
-
-pcap_path = "/home/jpdube/hull-voip/db/pcap"
+pcap_path = "/Users/jpdube/hull-voip/db/pcap"
+index_path = "/Users/jpdube/hull-voip/db"
 
 PCAP_GLOBAL_HEADER_SIZE = 24
 PCAP_PACKET_HEADER_SIZE = 16
 
-
 class Indexer:
-    def __init__(self, filename):
-        self.pcap_file = filename
-        self.index_name = os.path.splitext(filename)[0] + ".db"
+    def __init__(self):
+        self.index_name = f"{index_path}/index3.db"
+        print(f"Index filename is: {self.index_name}")
 
-    def pcap_read(self):
+    def pcap_read(self, file_id):
         result = []
+        count = 0
 
         start_time = datetime.now()
-
-        with open(self.pcap_file, "rb") as f:
+        pcap_file = f"{pcap_path}/{file_id}.pcap"
+        pd = PacketDecode()
+        with open(pcap_file, "rb") as f:
             _ = f.read(PCAP_GLOBAL_HEADER_SIZE)
 
             while True:
@@ -34,92 +31,105 @@ class Indexer:
                     break
                 timestamp = unpack("!I", header[0:4])[0]
                 incl_len = unpack("!I", header[12:16])[0]
-                # hl = int(header[16])
-                # packet = f.read(hl)
-                # print(f"{format_hex(packet)}")
-                # f.seek(incl_len - hl, 1)
                 packet = f.read(incl_len)
 
-                pb = PacketBuilder()
-                pb.from_bytes(packet)
+                pd.decode(packet)
 
-                self.insert(pb, f.tell(), timestamp)
+                pkt = [pd.ip_src, pd.ip_dst, pd.mac_src, pd.mac_dst, pd.ethertype,
+                       pd.ip_proto, pd.vlan_id, pd.sport, pd.dport, f.tell(), timestamp]
 
-        print(f"Found:{len(result)}, Time:{datetime.now() - start_time}")
-        return result
+                packet_info = { "pkt": pkt }    
+                # print(f"{pd.ip_proto:x}")
+                if pd.ip_proto == 0x06:
+                    tcp = [pd.tcp_ack_no, pd.tcp_seq_no, pd.tcp_flag]
+                    packet_info["tcp"] = tcp
 
-    def insert(self, pb: PacketBuilder, ptr: int, timestamp: int):
-        mac_dst = 0
-        mac_src = 0
-        ethertype = 0
-        vlan_id = 1
-        ip_dst = 0
-        ip_src = 0
-        ip_proto = 0
-        sport = 0
-        dport = 0
+                result.append(packet_info)
+                count += 1
 
-        e = pb.get_layer(LayerID.ETHERNET)
-        vlan_id = e.vlan_id
-        ethertype = e.ethertype
+                if len(result) == 4096:
+                    self.insert(result)
+                    result = []
 
-        ip = pb.get_layer(LayerID.IPV4)
+        self.insert(result)
+        exec_time = datetime.now() - start_time
+        print(
+            f"Found:{count}, Time:{exec_time}, Packet per sec: {count / exec_time.microseconds * 1_000_000:8}")
+        return count
 
-        if ip is not None:
-            ip_src = pb.get_field("ip.src").value
-            ip_dst = pb.get_field("ip.src").value
-            if ip.protocol == 0x11:
-                sport = pb.get_layer(LayerID.UDP).src_port
-                dport = pb.get_layer(LayerID.UDP).dst_port
-            elif ip.protocol == 0x06:
-                sport = pb.get_layer(LayerID.TCP).src_port
-                dport = pb.get_layer(LayerID.TCP).dst_port
+    def search_parallel(self):
+        pool = Pool()
+        flist = []
+        start_time = datetime.now()
+        for i in range(16):
+            flist.append(i)
+        # result = packet_search(params)
+        result = pool.map(self.indexer, flist)
 
-            ip_proto = ip.protocol
+        exec_time = datetime.now() - start_time
+        print(
+            f"---> Total Time: {exec_time}, Pkts count: {sum(result)}, Pkts per sec: {int(float(sum(result))/exec_time.total_seconds())}")
+        print("main script")
 
-        insert_stmt = f"""INSERT OR IGNORE INTO packet 
-                            (ip_src, 
-                            ip_dst, 
-                            mac_src, 
-                            mac_dst, 
-                            ether_type, 
-                            ip_proto, 
-                            vlan_id, 
-                            sport, 
-                            dport, 
-                            file_ptr, 
-                            timestamp)
-                         VALUES({ip_src},
-                                {ip_dst},
-                                {pb.get_field("eth.src").to_int()}, 
-                                {pb.get_field("eth.dst").to_int()}, 
-                                {ethertype}, 
-                                {ip_proto}, 
-                                {vlan_id}, 
-                                {sport}, 
-                                {dport}, 
-                                {ptr}, 
-                                {timestamp});  
-                            """
-        # print(insert_stmt)
-        self.cursor.execute(insert_stmt)
+    def insert(self, packet_info):
 
-    def indexer(self):
-        print(f"Base filename is: {self.index_name}")
+        for p in packet_info:
+            insert_stmt = f"""INSERT OR IGNORE INTO packet
+                                    (ip_src,
+                                    ip_dst,
+                                    mac_src,
+                                    mac_dst,
+                                    ether_type,
+                                    ip_proto,
+                                    vlan_id,
+                                    sport,
+                                    dport,
+                                    file_ptr,
+                                    timestamp)
+                                VALUES(?,?,?,?,?,?,?,?,?,?,?)"""
+            result = self.cursor.execute(insert_stmt, p["pkt"])
 
-        self.conn = sqlite3.connect(self.index_name)
+            if tcp_info := p.get("tcp", None):
+                insert_tcp = f"""INSERT OR IGNORE INTO tcp_packet
+                                        (packet_id,
+                                        ack,
+                                        syn,
+                                        tcp_flag
+                                        )
+                                    VALUES(?,?,?,?)"""
+                self.cursor.execute(insert_tcp, [result.lastrowid] + tcp_info)
+
+            # print(result.lastrowid)
+
+        self.conn.commit()
+
+    def indexer(self, file_id):
+        # index_name = f"{pcap_path}/{file_id}.db"
+        # print(f"Base filename is: {index_name}")
+
+        self.conn = sqlite3.connect(":memory")
+        # self.conn = sqlite3.connect(self.index_name)
         self.cursor = self.conn.cursor()
 
+        # PRAGMA cache_size = 1000000;
+        # PRAGMA temp_store = MEMORY;
+        # PRAGMA threads=8;
+        # PRAGMA locking_mode = EXCLUSIVE;
+        # PRAGMA journal_mode = WAL;
         self.cursor.executescript("""
-                PRAGMA journal_mode = MEMORY;
                 PRAGMA synchronous = OFF;
-                PRAGMA cache_size = 1000000;
                 PRAGMA temp_store = MEMORY;
-                PRAGMA threads=8;
-                PRAGMA locking_mode = EXCLUSIVE;
                 """)
+
+        self.create_tables()
+        # self.conn.execute("DROP TABLE if exists packet;")
+
+        count = self.pcap_read(file_id)
+        return count
+
+    def create_tables(self):
         self.conn.execute("""create table if not exists packet(
-                id integer not null primary key, 
+                id integer not null primary key,
                 ip_src integer,
                 ip_dst integer,
                 mac_src integer,
@@ -133,5 +143,14 @@ class Indexer:
                 timestamp timestamp
                 )""", [],)
 
-        self.pcap_read()
+        self.conn.execute("""create table if not exists tcp_packet(
+                id integer not null primary key,
+                packet_id integer not null,
+                ack integer,
+                syn integer,
+                tcp_flag integer,
+                window_size integer,
+                foreign key (packet_id) references packet(id) on delete cascade
+
+                )""", [],)
         self.conn.commit()
