cc862895cefa98e3ef510ab3812bfa1292bba145
JP Dube <jpdube@numa-informatique.com>
2022-11-05 14:54:47 -0400

    Modified main to test raw search

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from api.server2 import start
 from pql.interp import interpret_program
-from pql.pcapfile import PcapFile
 from pql.parse import parse_source
 from pql.interp_raw import interpret_program
 from datetime import datetime
@@ -8,7 +7,7 @@ from datetime import datetime
 
 def search():
     model = parse_source(
-        "select * from a where ip.src == 192.168.3.0/24 and tcp.syn == true;")
+        "select * from a where ip.src == 192.168.250.10/24 and ip.dst != 192.168.53.128/25 and ip.tos == IP_TOS_EF;")
     # "select * from ip.src where (ip.src == 8.8.8.8 and ip.dst == 192.168.3.230) or (ip.src == 192.168.3.230 and ip.dst == 8.8.8.8);")
     print(model)
 
@@ -21,15 +20,4 @@ def search():
 
 
 if __name__ == "__main__":
-    # pc = PcapFile()
-    # pc.open(f"0")
-
-    # count = 0
-    # for p in pc.next():
-    #     count += 1
-    #     print(p)
-    #     # print(p.print_hex())
-
-    # print(count)
-    # start()
     search()
