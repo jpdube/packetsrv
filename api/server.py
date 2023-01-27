9d3485557cbd8bb45f2c734b36241a223282ef27
JP Dube <jpdube@numa-informatique.com>
2023-01-26 20:31:08 -0500

    Modified server and code cleanup

--- api/server.py
+++ api/server.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from dbase.exec_query import exec_query
 from pydantic import BaseModel
+from dbase.dbengine import DBEngine
+# import uvicorn
 
 app = FastAPI()
 
@@ -23,40 +25,46 @@ class SnifferStop(BaseModel):
 
 @app.post("/pql")
 async def exec_pql(pql: PqlRequest):
-    result = exec_query(pql.query)
+    db = DBEngine()
+    result = db.exec_parallel(pql.query)
+    # result = exec_query(pql.query)
     return {"result": result}
 
 
-@app.post("/sniffer/start")
-async def sniffer_start(cmd: SnifferStart):
-    result = cmd
-    return {"result": result}
+# @app.post("/sniffer/start")
+# async def sniffer_start(cmd: SnifferStart):
+#     result = cmd
+#     return {"result": result}
 
 
-@app.post("/sniffer/stop")
-async def sniffer_stop(cmd: SnifferStop):
-    result = cmd
-    return {"result": result}
+# @app.post("/sniffer/stop")
+# async def sniffer_stop(cmd: SnifferStop):
+#     result = cmd
+#     return {"result": result}
 
 
-@app.get("/sniffer/show")
-async def sniffer_show():
-    result = [
-        {
-            "name": "sniffer_01",
-            "state": "running",
-            "iface": "en0",
-            "file_size": "2G",
-            "rotation_count": 16,
-            "alias": "trunk"
-        },
-        {
-            "name": "sniffer_02",
-            "state": "running",
-            "iface": "en1",
-            "file_size": "2G",
-            "rotation_count": 16,
-            "alias": "firewall"
-        }
-    ]
-    return {"result": result}
+# @app.get("/sniffer/show")
+# async def sniffer_show():
+#     result = [
+#         {
+#             "name": "sniffer_01",
+#             "state": "running",
+#             "iface": "en0",
+#             "file_size": "2G",
+#             "rotation_count": 16,
+#             "alias": "trunk"
+#         },
+#         {
+#             "name": "sniffer_02",
+#             "state": "running",
+#             "iface": "en1",
+#             "file_size": "2G",
+#             "rotation_count": 16,
+#             "alias": "firewall"
+#         }
+#     ]
+#     return {"result": result}
+
+
+# def start():
+#     uvicorn.run(app=app, host="localhost", port=8080)
