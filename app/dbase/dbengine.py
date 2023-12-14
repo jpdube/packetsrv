b9f8de45da20b22642a950a03ef1d24e2360adfa
JP Dube <jpdube@numa-informatique.com>
2023-12-14 18:21:28 -0500

    Improved performance of packet reading
    - Dbengine now reads packet in parallel

--- app/dbase/dbengine.py
+++ app/dbase/dbengine.py
@@ -1,8 +1,9 @@
+import multiprocessing as mp
 from datetime import datetime
 from ipaddress import IPv4Address
+from typing import Any, Generator
 
-from dbase.index_manager import IndexManager
-from packet.layers.fields import IPv4Address
+from dbase.index_manager import IndexManager, PktPtr
 from packet.layers.packet_builder import PacketBuilder
 from pql.interp_raw import exec_program
 from pql.parse import parse_source
@@ -26,37 +27,59 @@ class DBEngine:
         index_result = self.index_mgr.search(
             self.model.index_field, self.model.ip_list)
         # index_result = self.index_mgr.search(field_index, self.model.ip_list)
-        count = 0
         searched = 0
         self.pkt_found = 0
 
         print(self.model.has_interval)
         print(self.model.interval)
 
-        for idx in index_result:
-            for i in idx:
-                searched += 1
-                pkt_result = exec_program(self.model.where_expr, i)
-                if pkt_result is not None:
-                    pb = PacketBuilder()
-                    pb.from_bytes(pkt_result.packet, pkt_result.header)
-                    self.get_fields(pb)
-                    count += 1
-                    self.pkt_found += 1
+        search_result = []
 
-                if count == self.model.top_expr:
+        pool = mp.Pool()
+        for idx in index_result:
+            params = []
+            for chunk_pkt in self.chunks(idx, mp.cpu_count()):
+                for pkt in chunk_pkt:
+                    params.append((pkt, self.model.where_expr))
+
+                result = pool.starmap(self.search_pkt, params)
+                for r in result:
+                    searched += 1
+                    if r is not None:
+                        search_result.append(r)
+                        self.pkt_found += 1
+
+                    if self.pkt_found >= self.model.top_expr:
+                        break
+
+                if self.pkt_found >= self.model.top_expr:
                     break
 
-            if count == self.model.top_expr:
+            if self.pkt_found >= self.model.top_expr:
                 break
 
         ttl_time = datetime.now() - start_time
         print(self.model.select_expr)
 
         print(
-            f"---> Scaneed: {searched} in Time: {ttl_time} Result: {self.pkt_found} TOP: {self.model.top_expr} SELECT: {self.model.select_expr}")
+            f"---> Index scan in Time: {ttl_time} Result: {searched}:{self.pkt_found} TOP: {self.model.top_expr} SELECT: {self.model.select_expr}")
+
+        for p in search_result:
+            self.get_fields(p)
 
         return self.result
+
+    def search_pkt(self, pkt_ptr: PktPtr, where_expr):
+        pkt_result = exec_program(where_expr, pkt_ptr)
+        if pkt_result is not None:
+            pb = PacketBuilder()
+            pb.from_bytes(pkt_result.packet, pkt_result.header)
+            self.get_fields(pb)
+            return pb
+
+    def chunks(self, l: list[Any], n: int) -> Generator[Any, Any, Any]:
+        for i in range(0, len(l), n):
+            yield l[i:i + n]
 
     def process(self, index_result):
         ...
