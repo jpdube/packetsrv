fd2dee365603fcf335cc7a00c8c50fa7fa5e8a62
JP Dube <jpdube@numa-informatique.com>
2024-01-07 12:07:35 -0500

    Code cleanup for sniffer server

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from api.server import start
 from config.config import Config
+from dbase.dbengine import DBEngine
 
 if __name__ == "__main__":
     Config.load()
+    db = DBEngine()
+    # db.index_db()
     start()
--- app/server/sniffer.py
+++ app/server/sniffer.py
@@ -1,16 +1,19 @@
-import subprocess
 import os
-import time
 import signal
+import subprocess
+import time
+
 import psutil
+
 
 class Sniffer:
     def __init__(self):
         ...
 
     def start(self, name: str, iface: str, username: str):
-        output_dir = f"/home/jpdube/hull-voip/sniffer/{name}/"
-        cmd = f"tcpdump -i {iface} -C 2 -W 5 -w {output_dir} -Z {username} -z {output_dir}move.sh"
+        output_dir = f"/Users/jpdube/sniffer/{name}/"
+        cmd = f"tcpdump -i {iface} -C 2 -W 5 -w {
+            output_dir} -Z {username} -z {output_dir}move.sh"
         print(f"CMD: {cmd}")
         p = subprocess.Popen(cmd, shell=True)
         print(f"Sniffer {name} PID: {p.pid}")
@@ -21,14 +24,15 @@ class Sniffer:
         pass
 
     def status(self, pid: int):
-        process_status = [ proc for proc in psutil.process_iter() if proc.pid == pid ]
+        process_status = [
+            proc for proc in psutil.process_iter() if proc.pid == pid]
         if process_status:
             for current_process in process_status:
                 return current_process.status()
         else:
-           return None 
+            return None
 
 
 if __name__ == "__main__":
     sniffer = Sniffer()
-    sniffer.start("trunk", "wlp0s20f3", "jpdube")
+    sniffer.start("trunk", "en11", "jpdube")
