cfa5fadb59ba749fef1123a2e97db3e05f3d1fd4
JP Dube <jpdube@numa-informatique.com>
2024-02-20 10:58:28 -0500

    Added multi threaded support for packet import
    - To be able to sustain the wire speed a parallel algorithm was written to process packet files

--- app/server/file_monitor.py
+++ app/server/file_monitor.py
@@ -3,14 +3,17 @@ from watchdog.events import FileSystemEventHandler
 import time
 import os
 from threading import Thread
-from queue import Queue
+import multiprocessing as mp
+import glob
+
 from config.config_db import ConfigDB
 from pql.pcapfile import PcapFile
 import logging
 
+
 log = logging.getLogger("packetdb")
 
-capture_queue = Queue()
+capture_queue = mp.Queue()
 
 
 class _Handler(FileSystemEventHandler):
@@ -42,7 +45,7 @@ class PacketDbWatch:
         self.observer.start()
         try:
             while True:
-                time.sleep(1.5)
+                time.sleep(0.5)
         except:
             self.observer.stop()
             log.info("Observer Stopped")
@@ -50,29 +53,39 @@ class PacketDbWatch:
         self.observer.join()
 
 
-def capture_thread():
+def capture_thread(in_queue: mp.Queue):
     configdb = ConfigDB()
+    pool = mp.Pool()
     while True:
-        pcap_file = capture_queue.get()
+        flist = []
+        new_file = in_queue.get()
         file_id = configdb.next_id
-        log.debug(f"Next file id: {file_id} -- Capture file: {pcap_file}")
-        os.rename(pcap_file, f"/Users/jpdube/pcapdb/db/pcap/{file_id}.pcap")
-        pcap = PcapFile()
-        mx_idx = pcap.create_index(file_id)
-        master_idx = []
-        master_idx.append(mx_idx)
-        pcap.build_master_index(master_idx)
+        log.debug(f"Next file id: {file_id} -- Capture file: {new_file}")
+        os.rename(new_file, f"/opt3/capture/pcap/{file_id}.pcap")
+        flist.append(file_id)
+        file_count = in_queue.qsize()
+        for f in range(file_count):
+            new_file = in_queue.get()
+            file_id = configdb.next_id
+            log.debug(f"Next file id: {file_id} -- Capture file: {new_file}")
+            os.rename(new_file, f"/opt3/capture/pcap/{file_id}.pcap")
+            flist.append(file_id)
+
+        log.debug(f"File list: {flist}")
+        pcapfile = PcapFile()
+        result = pool.map(pcapfile.create_index, flist)
+        result.sort(key=lambda a: a[0])
+        pcapfile.build_master_index(result)
 
 
 def on_new_pcap(src_filename: str):
     capture_queue.put(src_filename)
-    # os.rename()
 
 
 def start_db_watcher(watch_dir: str):
     log.info(f"Database watcher started with folder: {watch_dir}")
     dbwatch = PacketDbWatch(watch_dir)
-    Thread(target=capture_thread, daemon=True).start()
+    mp.Process(target=capture_thread, args=(capture_queue,)).start()
     Thread(target=dbwatch.run, daemon=True).start()
 
 
