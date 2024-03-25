6edac71105695835d1aa00007f44f6a018503e6e
JP Dube <jpdube@numa-informatique.com>
2024-03-25 14:45:51 -0400

    Moved from threads to multiprocessing for packet capture parallel algorithm

--- app/server/file_monitor.py
+++ app/server/file_monitor.py
@@ -18,18 +18,21 @@ capture_queue = mp.Queue()
 
 class _Handler(FileSystemEventHandler):
 
+    def __init__(self, capture_queue):
+        self.capture_queue = capture_queue
+
     def on_created(self, event):
         log.debug(f"on_created {event.src_path}")
-        on_new_pcap(event.src_path)
+        self.capture_queue.put(event.src_path)
 
     def on_deleted(self, event):
-        log.debug(f"on_deleted {event.src_path}")
+        pass
 
-    # def on_modified(self, event):
-    #     log.debug(f"on_modified {event.src_path}")
+    def on_modified(self, event):
+        pass
 
     def on_moved(self, event):
-        log.debug(f"on_moved {event.src_path}")
+        pass
 
 
 class PacketDbWatch:
@@ -38,10 +41,10 @@ class PacketDbWatch:
         self.watchDirectory = packet_dir
         self.observer = Observer()
 
-    def run(self):
-        event_handler = _Handler()
+    def run(self, capture_queue: mp.Queue):
+        event_handler = _Handler(capture_queue)
         self.observer.schedule(
-            event_handler, self.watchDirectory, recursive=True)
+            event_handler, self.watchDirectory, recursive=False)
         self.observer.start()
         try:
             while True:
@@ -53,20 +56,20 @@ class PacketDbWatch:
         self.observer.join()
 
 
-def capture_thread(in_queue: mp.Queue):
+def capture_thread(in_queue: mp.Queue, profile_id: int):
     configdb = ConfigDB()
     pool = mp.Pool()
     while True:
         flist = []
         new_file = in_queue.get()
-        file_id = configdb.next_id
+        file_id = configdb.capture_next_id(profile_id)
         log.debug(f"Next file id: {file_id} -- Capture file: {new_file}")
         os.rename(new_file, f"/opt3/capture/pcap/{file_id}.pcap")
         flist.append(file_id)
         file_count = in_queue.qsize()
         for f in range(file_count):
             new_file = in_queue.get()
-            file_id = configdb.next_id
+            file_id = configdb.capture_next_id(profile_id)
             log.debug(f"Next file id: {file_id} -- Capture file: {new_file}")
             os.rename(new_file, f"/opt3/capture/pcap/{file_id}.pcap")
             flist.append(file_id)
@@ -78,15 +81,11 @@ def capture_thread(in_queue: mp.Queue):
         pcapfile.build_master_index(result)
 
 
-def on_new_pcap(src_filename: str):
-    capture_queue.put(src_filename)
-
-
-def start_db_watcher(watch_dir: str):
+def start_db_watcher(watch_dir: str, profile_id: int):
     log.info(f"Database watcher started with folder: {watch_dir}")
     dbwatch = PacketDbWatch(watch_dir)
-    mp.Process(target=capture_thread, args=(capture_queue,)).start()
-    Thread(target=dbwatch.run, daemon=True).start()
+    mp.Process(target=capture_thread, args=(capture_queue, profile_id)).start()
+    mp.Process(target=dbwatch.run, args=(capture_queue,), daemon=True).start()
 
 
 if __name__ == '__main__':
