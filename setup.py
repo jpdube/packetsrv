f263021b44a85dd43781b715ae2be6931e211d26
JP Dube <jpdube@numa-informatique.com>
2024-04-05 14:46:09 -0400

    Modified cpython files to compile

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 
 setup(
     name='Packet lib',
-    ext_modules=cythonize("./app/packet/layers/packet_decode.pyx", annotate=True,
+    ext_modules=cythonize(("./app/packet/layers/ethernet.pyx", "./app/packet/layers/packet.pyx", "./app/packet/layers/ipv4.pyx", "./app/packet/layers/tcp.pyx"), annotate=True,
                           compiler_directives={"language_level": 3}),
 )
